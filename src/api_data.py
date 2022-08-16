b967408e21441c36a930e04a4e3b8be4b79610c3
albertferre <albertferrevidal@gmail.com>
2022-08-16 18:16:44 +0200

    black and pylint formatting

--- src/api_data.py
+++ src/api_data.py
@@ -1,25 +1,36 @@
-import requests
+""" This module contains the methods to fetch data from the CoinMarketCap API. """
+
 import json
 import logging
-import pickle
+import requests
 
 log = logging.getLogger(__name__)
 
-def coinmarket_api_data(api_key:str) -> dict:
 
-    log.info('Fetching data from Coinmarketcap API')
-    headers = {'X-CMC_PRO_API_KEY': api_key}
+def coinmarket_api_data(api_key: str) -> dict:
+    """This method calls the CoinMarketCap API and returns the data.
+
+    :param api_key: The coinMarketCap API key.
+    :type api_key: str
+    :raises Exception: If the API key is not valid.
+    :return: The data from the CoinMarketCap API.
+    :rtype: dict
+    """
+
+    log.info("Fetching data from Coinmarketcap API")
+    headers = {"X-CMC_PRO_API_KEY": api_key}
     response = requests.get(
-        'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC',
-        headers=headers
-        )
+        "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC",
+        headers=headers,
+    )
     if response.status_code != 200:
-        raise Exception('Error fetching data from Coinmarketcap API')
-    else:
-        log.info("Data fetched from Coinmarketcap API successfully")
-        data = json.loads(response.text)
+        raise Exception("Error fetching data from Coinmarketcap API")
+
+    log.info("Data fetched from Coinmarketcap API successfully")
+    data = json.loads(response.text)
 
     return data
 
-#if __name__ == '__main__':
-    #print(coinmarket_api_data('6db7fc06-6935-4797-8bc5-8cfa2d80ad1f'))
+
+# if __name__ == '__main__':
+# print(coinmarket_api_data('6db7fc06-6935-4797-8bc5-8cfa2d80ad1f'))
--- src/db_dumper.py
+++ src/db_dumper.py
@@ -1,20 +1,30 @@
+"""This module contains the functions to dump data into a MongoDB database."""
+import logging
 from pymongo import MongoClient
 
+log = logging.getLogger(__name__)
 
-def dump(json_data: dict, db_name: str='nuwe', collection_name: str='coinmarketcap'):
+
+def dump(
+    json_data: dict, db_name: str = "nuwe", collection_name: str = "coinmarketcap"
+):
+    """This function dumps the data into a MongoDB database.
+
+    :param json_data: The data to be dumped.
+    :type json_data: dict
+    :param db_name: ds name, defaults to "nuwe"
+    :type db_name: str, optional
+    :param collection_name: collection name, defaults to "coinmarketcap"
+    :type collection_name: str, optional
+    """
+    log.info("Connecting to MongoDB")
     client = MongoClient()
 
     dblist = client.list_database_names()
     if "my_database" in dblist:
-        print("The database exists.")
+        log.info("Database exists")
     mydb = client[db_name]
     mycol = mydb[collection_name]
 
-    #insert a document to the collection
-    x = mycol.insert_one(json_data)
-    print(x)
-
-
-if __name__ == "__main__":
-    json_data = { "name": "Saranya", "address": "Kochi" }
-    dump(json_data)
+    log.info("inserting data into MongoDB")
+    mycol.insert_one(json_data)
